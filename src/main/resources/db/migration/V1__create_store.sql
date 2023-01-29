dd8a8a23cc43a8e45b988534a288ebe6bfa451cb
kurt112 <kurtorioque112@gmail.com>
2023-01-29 18:42:01 +0800

    update db

--- src/main/resources/db/migration/V1__create_store.sql
+++ src/main/resources/db/migration/V1__create_store.sql
@@ -1,4 +1,5 @@
-# insert into store(id,longitude,latitude,name,radius,scope_color,scope_edge_color) values (1,'14.676109','121.134693','Default Name','200','green','black');
+insert into store(id,longitude,latitude,name,radius,scope_color,scope_edge_color) values (1,'121.134693','14.676109','Default Name','200','green','black');
 
+#password - erikbikeshop
 # no password admin
-#  INSERT INTO `bike-rental`.user (id, birthdate, cellphone, created_at, email, first_name, gender, is_account_not_expired, is_account_not_locked, is_credential_not_expired, is_enable, last_name, middle_name, user_password, updated_at, role) VALUES (0, '2023-01-08 04:05:53.000000', '09617134338', '2023-01-08 04:06:08.000000', 'admin@email.com', 'kurt', null, true, true, true, true, 'admin', 'true', '', '2023-01-08 04:06:50.000000', 'admin')
+INSERT INTO `bike-rental`.user (id, birthdate, cellphone, created_at, email, first_name, gender, is_account_not_expired, is_account_not_locked, is_credential_not_expired, is_enable, last_name, middle_name, user_password, updated_at, role) VALUES (0, '2023-01-08 04:05:53.000000', '09617134338', '2023-01-08 04:06:08.000000', 'admin@email.com', 'erik', null, true, true, true, true, 'admin', 'bikeshop', '$2a$10$Pi2RbX6El53f5pEaxjcXguiB6t0Gyppx6iQX7OM2de4ZvygQfRVI.', '2023-01-08 04:06:50.000000', 'admin');
